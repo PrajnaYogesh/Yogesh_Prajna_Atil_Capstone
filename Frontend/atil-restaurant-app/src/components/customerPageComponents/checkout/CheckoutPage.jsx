9f86207d504188a7688ba75365bc97fbf082f9ee
Prajna <prajna.yogish4@gmail.com>
2024-10-08 18:08:02 -0400

    checkout page complete

--- Frontend/atil-restaurant-app/src/components/customerPageComponents/checkout/CheckoutPage.jsx
+++ Frontend/atil-restaurant-app/src/components/customerPageComponents/checkout/CheckoutPage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './CheckoutPage.css'
+import './CheckoutPage.css'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function CheckoutPage() {
+
+  const [cart, setCart] = useState(null);
+  const cartId = localStorage.getItem('cartId') || '';
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCart = async () => {
+    try {
+      console.log(cartId);
+      const response = await axios.get(`http://localhost:3000/cart/carts/${cartId}`);
+      console.log('items from the cart is',response)
+      setCart(response.data);
+    } catch (error) {
+      console.error('Error fetching cart:', error);
+    }  
+     
+    };
+
+    fetchCart();
+  }, [cartId]);
+
+  const handleCheckout = async () => {
+try {
+  await axios.post(`http://localhost:3000/cart/carts/${cartId}/checkout`);
+  alert('Payment successful'); // Show success message
+  navigate('/'); // Navigate to home page
+} catch (error) {
+  console.error('Checkout failed:', error);
+        alert('Checkout failed. Please try again.');
+} 
+  };
+
+  if (!cart) return <div>Loading...</div>;
+
   return (
-    <div>CheckoutPage</div>
+    <div className='cartOuterContainer'>
+      <h2>Your Cart</h2>
+      {cart.items.map((item, index) => (
+        <div key={index}>
+          <p>{item.itemName}</p>
+          <p>Quantity: {item.quantity}</p>
+          <p>Price: {item.pricePerItem}</p>
+        </div>
+      ))}
+      <h3>Total Amount: {cart.totalAmount}</h3>
+      <button onClick={handleCheckout}>Checkout</button>
+    </div>
   )
 }
 
